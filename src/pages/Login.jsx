a0e295d73898d3e069cdfa7eb7aca392641befc3
ekyeon715 <rkdwldus9636@naver.com>
2024-08-31 08:56:04 +0900

    feat: 깃허브 로그인 추가

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import supabase from '../../suparbase';
 import styled from 'styled-components';
@@ -16,6 +16,21 @@ const Login = () => {
     if (error) {
       alert('아이디와 비밀번호를 다시 확인해주세요');
     } else {
+      navigate('/mainnewsfeed');
+    }
+  };
+
+  // GitHub로 로그인하는 함수
+  const handleGithubLogin = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'github' // GitHub OAuth 프로바이더 사용
+    });
+
+    if (error) {
+      // 오류가 발생하면 알림을 표시합니다.
+      alert('GitHub 로그인에 실패했습니다. 다시 시도해주세요.');
+    } else {
+      // 성공하면 메인 뉴스 피드 페이지로 이동합니다.
       navigate('/mainnewsfeed');
     }
   };
@@ -43,6 +58,8 @@ const Login = () => {
           }}
         />
       </InputSection>
+
+      {/* 회원가입 페이지로 이동하는 버튼 */}
       <button
         onClick={() => {
           navigate('/loginaccount');
@@ -54,6 +71,12 @@ const Login = () => {
       <br></br>
       <br></br>
       <button onClick={handleLogin}>login(메인화면 로직으로 이동)</button>
+
+      <br></br>
+      <br></br>
+
+      {/* GitHub로 로그인하는 버튼 */}
+      <button onClick={handleGithubLogin}>GitHub로 로그인</button>
     </>
   );
 };
