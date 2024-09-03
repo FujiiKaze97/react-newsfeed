64007c51b10c40c753822e3512df833169256ece
ekyeon715 <rkdwldus9636@naver.com>
2024-09-04 00:57:27 +0900

    style: 회원가입 페이지 수정

--- src/pages/LoginAccount.jsx
+++ src/pages/LoginAccount.jsx
@@ -2,6 +2,17 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import supabase from '../../supabase';
+import {
+  InputContainer,
+  InputForm,
+  InputLabel,
+  LeftSection,
+  LoginButton,
+  RightSection,
+  WelcomeImage,
+  WelcomeText,
+  Wrapper
+} from './LoginStyle';
 
 const LoginAccount = () => {
   const navigate = useNavigate();
@@ -25,36 +36,51 @@ const LoginAccount = () => {
       console.log(e);
     }
   };
-  
+
   return (
     <div>
-      <h1>회원가입 페이지 입니다</h1>
-      <InputSection>
-        <input
-          type="email"
-          placeholder="이메일입력"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="비밀번호입력"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-      </InputSection>
-      <button
-        onClick={() => {
-          navigate('/');
-        }}
-      >
-        로그인
-      </button>
-      <button onClick={handleSignUp}>회원가입</button>
+      <Wrapper>
+        <LeftSection>
+          <WelcomeImage src="https://cdn.imweb.me/upload/S20220728586c4366ae600/1b44908f5637f.png" alt="환영 이미지" />
+          <WelcomeText>어서오시개!</WelcomeText>
+        </LeftSection>
+
+        <RightSection>
+          <h1>회원가입 페이지 입니다</h1>
+          <InputContainer>
+            <InputLabel>이메일</InputLabel>
+
+            <InputForm
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </InputContainer>
+
+          <InputContainer>
+            <InputLabel>비밀번호</InputLabel>
+            <InputForm
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </InputContainer>
+
+          <LoginButton
+            onClick={() => {
+              navigate('/');
+            }}
+          >
+            로그인
+          </LoginButton>
+
+          <button onClick={handleSignUp}>회원가입</button>
+        </RightSection>
+      </Wrapper>
     </div>
   );
 };
